e3e9983adfc55f96742ba20c3b6edc827a2ff6e3
swalihhaneef <swalihsr7l@gmail.com>
2026-05-23 16:13:58 +0530

    frontend urls updated

--- config.js
+++ config.js
@@ -9,3 +9,9 @@ export const DATABASE_URL = process.env.DATABASE_URL;
 if (!DATABASE_URL) {
   throw new Error("DATABASE_URL must be defined");
 }
+
+export const frontendUrls = [
+  "https://www.lunevia.in/",
+  "https://lunevia.vercel.app/",
+  "http://localhost:3000",
+]
