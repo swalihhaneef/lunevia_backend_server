df74f4abe36a31bd076ecdf3c921bea48bb4a652
swalihhaneef <swalihsr7l@gmail.com>
2026-05-23 12:54:24 +0530

    Fix production env config

--- config.js
+++ config.js
@@ -1,15 +1,8 @@
 import { config } from "dotenv";
-import fs from "fs";
+
+config();
 
 export const PORT = process.env.PORT || 4000;
-
-if (!fs.existsSync(".env") && !fs.existsSync(".env.development")) {
-  throw new Error("Missing both .env and .env.local files. At least one is required.");
-}
-
-const env = fs.existsSync(".env.development") ? ".env.development" : ".env";
-
-config({ path: env });
 
 export const DATABASE_URL = process.env.DATABASE_URL;
 
